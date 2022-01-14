2326077dd254078bdc08463b3d591c2cfd3194a5
nolfla <44060336+nolfla@users.noreply.github.com>
2022-01-13 18:23:36 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class Hello
+{
+public static void main(String[] args)
+{
+int i = 1;
+ 
+while (i < 100) {
+  
+  System.out.println("Hello, I.T. Have you tried turning it off and on again?");
+  
+  i++;
+  
+}
+
+}
+}
